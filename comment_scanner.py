f970406b876ae6817dee018c9f402487b534a4f8
Chaseshak <chaseshak@gmail.com>
2017-01-31 18:34:26 -0600

    Begin refactoring into multiple files. Will use DB to prevent rate limiting

--- comment_scanner.py
+++ comment_scanner.py
@@ -1,3 +1,6 @@
+# This py script scans /r/all periodically (Once every 2 hours executed by crontab)
+# and imports the comments to reply to in a database
+# To see what happens once a row is in the DB, see comment_reply.py
 import praw
 import config
 
@@ -17,23 +20,27 @@ reddit = praw.Reddit(user_agent="MockSaluteBot (by /u/chaseshak)",
                      username=config.uName, password=config.password)
 
 # Set to /r/HIMYM for least likely to be banned for many comments
-subreddit = reddit.subreddit("HIMYM")
+subreddit = reddit.subreddit("all")
 
 
 def reply_to_comment(comment, word, to_reply):
 
     # Safety check the to_reply word for any odd characters
-    to_reply = to_reply.translate({ord(i): None for i in '!@#$\"'})
+    to_reply = to_reply.translate({ord(i): None for i in '!@#$\".'})
 
     # Create the mock salute
-    mock_salute = mapping[word] + " " + to_reply.title() + "!"
+    mock_salute = mapping[word] + " " + to_reply.title() + "!" + " (｀-´)>"
 
     # Formatted comment reply for Reddit
-    comment_reply = mock_salute + "  \n&nbsp;  \n^(I am a bot. This comment was auto-generated. To learn more " \
+    comment_reply = mock_salute + "  \n&nbsp;  \n^(I am a bot. Mock Salutes are a joke from) " \
+                                  "[^HIMYM](http://how-i-met-your-mother.wikia.com/wiki/Mock_Salutes)^. ^( This"\
+                                  " comment was auto-generated. To learn more " \
                                   "about me, see my) [^github ^page](https://github.com/Chaseshak/MockSalutesBot)^."
 
-    # Reply to the comment
-    comment.reply(comment_reply)
+    print(comment_reply)
+
+    # Insert into DB
+    # comment.reply(comment_reply)
 
     return
 
@@ -66,7 +73,7 @@ def check_body(comment):
                 reply_to_comment(comment, word, tokens[index_word + 1])
 
 
-for submission in subreddit.hot(limit=10):
+for submission in subreddit.hot(limit=20):
     comments = submission.comments.list()
     for comment in comments:
         check_body(comment)
--- config.template.py
+++ config.template.py
@@ -3,3 +3,8 @@ clientID = 'CLIENT_ID'
 secretID = 'SECRET_ID'
 uName = 'USERNAME'
 password = 'PASSWORD'
+
+db_host = 'IP_ADDR_DB'
+db_name = 'MockSalutes'
+db_user = 'DB_USER'
+db_pass = 'DB_PASSWORD'
