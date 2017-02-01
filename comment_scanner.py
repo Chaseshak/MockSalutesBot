108a263bff9f9bae398c6f3b9ab62c39a09fe939
Chaseshak <chaseshak@gmail.com>
2017-01-31 23:16:10 -0600

    Change subreddits in hot to only 3

--- comment_scanner.py
+++ comment_scanner.py
@@ -88,7 +88,7 @@ def check_body(comment):
 
 
 # Go through 5 most recent /r/all hot posts. called hourly by cron
-for submission in subreddit.hot(limit=5):
+for submission in subreddit.hot(limit=3):
     comments = submission.comments.list()
     for comment in comments:
         check_body(comment)
